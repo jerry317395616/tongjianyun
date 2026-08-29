00874d77ede5518f7b9c3c1082991e4ce49f2b40
jerry317395616 <317395616@qq.com>
2026-08-29 12:28:30 +0800

    normalize recipe day doctype path

---
+++ tongjianyun/tongjianyun/doctype/tongjianyun_recipe_day/tongjianyun_recipe_day.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Local and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tongjianyun Recipe Day", {
+// 	refresh(frm) {
+
+// 	},
+// });
